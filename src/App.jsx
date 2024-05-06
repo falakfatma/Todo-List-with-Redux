56201364be1a592509669e70aa4782ecc82e9c3e
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2024-05-06 11:15:29 +0000

    card UI is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 
 export default function App() {
   return (
-    <main>
-      <h4>Todo List</h4>
+    <main className="flex bg-gray-50 translate-y-5 h-[30rem] w-[30rem] m-auto rounded shadow-md drop-shadow-2xl">
+      <div className="m-[2rem]">
+        <h4 className="text-xl font-bold">Todo List </h4>
+        <input className="w-[23rem] rounded bg-gray-100"/>
+      </div>
     </main>
   );
 }
